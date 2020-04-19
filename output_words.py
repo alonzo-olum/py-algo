99d670db759f256218fcb09684bb6c68059795a0
Alonzo <alondiff@gmail.com>
2020-04-19 19:34:51 +0300

    optimized dry challenge

---
+++ output_words.py
@@ -0,0 +1,17 @@
+def output_words(num):
+    ones=['','one', 'two', 'three', 'four','five','six','seven','eight',
+            'nine','ten','eleven','twelve','thirtheen','fourteen','fifteen',
+            'sixteen','seventeen','eighteen','nineteen']
+    tens=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    thousands={100:'hundred',1000:'thousand',1000000:'million'}
+    if num<20:
+        return ones[num]
+    if num<100:
+        return tens[num/10-2] + ( '' if num%10 == 0 else ' ' + ones[num%10] )
+    #finding the largest key in num subset
+    maximumKey=max([key for key in thousands.keys() if key<=num])
+    return output_words(num/maximumKey) +' '+ thousands[maximumKey] + ('' if num%maximumKey==0 else ' '+ output_words(num%maximumKey))
+
+#main block
+input=raw_input("Enter an integer\n")
+print output_words(int(input))
