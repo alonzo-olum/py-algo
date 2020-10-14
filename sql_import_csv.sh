ece1c391d1845f05d113d127b59cad95b5652b1b
Alonzo <alondiff@gmail.com>
2020-10-14 17:26:57 +0300

    fix script bugs

--- sql_import_csv.sh
+++ sql_import_csv.sh
@@ -1,19 +1,22 @@
 #!/bin/env bash
 
-mysql_import {
+
+function mysql_import {
   mysqlimport --local --fields-terminated-by=',' --lines-terminated-by='\r\n' $1 $2
 }
 
 # main block
-local db = ""
-while true\
-do
+
+database=""
+while true; do
   touch default_sql.csv
   for file in `find . -cnewer default_sql.csv | sed '$d'`
   do
-     epoch_time = stat -c'%Z' $file
+     epoch_time = `stat -c'%Z' $file`
      touch $epoch_time default_sql.csv
-     mysql_import $db  $file
+     mysql_import $database  $file
      if [ $? ]; then
-       echo ": an error occured "
-   done;
+       echo ": an error occured"
+     fi
+   done
+ done
